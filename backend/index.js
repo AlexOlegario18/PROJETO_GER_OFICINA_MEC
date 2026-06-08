1d68070436784a63dd73b35af5a8ec8530b8eae6
Maria lina <marialina9728@gmail.com>
2026-06-08 12:11:13 -0300

    corrigindo caminho do osControllers

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const osControllers = require('./osControllers');
+const osControllers = require('./controllers/osControllers');
 
 const app = express();
 app.use(cors());
